f89388927722b9b104ba70251cc5e8adee39115c
Sahil Mondal <sahil.mondal@datagrokr.com>
2024-09-09 23:01:56 +0530

    updated app and test app

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
 def add(a, b):
     return a + b
+def sub(c, d):
+    return c - d
--- test_app.py
+++ test_app.py
@@ -1,4 +1,8 @@
 from app import add
+from app import sub
 
 def test_add():
     assert add(1, 2) == 3
+
+def test_sub():
+    assert sub(7, 4) == 3
