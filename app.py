8bba8ef806200467bd3f77abb3b99c693f584194
Sahil Mondal <sahil.mondal@datagrokr.com>
2024-09-09 23:08:22 +0530

    added mult and div funcs

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ def add(a, b):
     return a + b
 def sub(c, d):
     return c - d
+def mult(e, f):
+    return e * f
+def div(g, h):
+    return g / h
--- test_app.py
+++ test_app.py
@@ -1,8 +1,13 @@
-from app import add
-from app import sub
+from app import add, sub, mult, div
 
 def test_add():
     assert add(1, 2) == 3
 
 def test_sub():
     assert sub(7, 4) == 3
+
+def test_mult():
+    assert mult(3, 4) == 12
+
+def test_div():
+    assert div(10, 2) == 5
